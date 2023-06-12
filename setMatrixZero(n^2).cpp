6aa9f02721728bffc8613c6622a5686e6fc349d3
ankitanjana23 <ankitanjana23@gamil.com>
2023-06-12 14:44:24 +0530

    first commit

---
+++ setMatrixZero(n^2).cpp
@@ -0,0 +1,49 @@
+/* more optimal solution n^2 */
+
+#include <iostream>
+#include <limits.h>
+#include <vector>
+using namespace std;
+
+void setZeroes(vector<vector<int>> &matrix)
+{
+    /*  O(n^3) Time complexity first technique if I am replace all 0 to -1
+    and Input array gives -1 in this case it will also replce with 0 dont Do it ! */
+
+    int n = matrix.size();    // number of rows
+    int m = matrix[0].size(); // row=0 so number of columns
+    int row[n] = {0};
+    int column[m] = {0}; // all element inside the row and coloumn zero
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                row[i] = 1;
+                column[j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (row[i] || column[j])
+            {
+                matrix[i][j] == 0;
+            }
+            cout << matrix[i][j] << ",";
+        }
+    }
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{-1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int m = matrix.size();
+    int n = matrix[0].size();
+    setZeroes(matrix);
+}
